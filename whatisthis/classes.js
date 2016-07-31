07a675487de165f0cb0a1a19e1948856e9cf4296
Camila San <smayres@gmail.com>
2016-07-31 16:08:02 +0200

    JS: making sense of inheritance in JS

--- whatisthis/classes.js
+++ whatisthis/classes.js
@@ -1,10 +1,17 @@
+Function.prototype.method = function (name, func) {
+    if (!this.prototype[name]) {
+        this.prototype[name] = func;
+        return this;
+    }
+};
+
 Function.method('new', function(){
 	var that = Object.create(this.prototype);
 	var other = this.apply(that, arguments);
 	return (typeof other === 'object' && other) || that;
 });
 
-var Mammal = function(){
+var Mammal = function(name){
 	this.name = name;
 };
 
@@ -18,4 +25,40 @@ Mammal.prototype.says = function(){
 
 var myMammal = new Mammal('Herb the Mammal');
 var name = myMammal.get_name();
-console.log(name);
+console.log('myMammal: ' + name);
+
+// var Cat = function (name) {
+//     this.name = name;
+//     this.saying = 'meow';
+// };
+// 
+// Cat.prototype = new Mammal();
+// Cat.prototype.purr = function (n) {
+//     var i, s = '';
+//     for (i = 0; i < n; i += 1) {
+//         if (s) {
+//             s += '-';
+//         }
+//         s += 'r';
+//     }
+//     return s;
+// };
+// 
+// var myCat = new Cat('Henrietta');
+// console.log('myCat: '+ myCat.get_name() + ' ' + myCat.says() + ' ' + myCat.purr(5));
+
+Function.method('inherits', function (Parent) {
+    this.prototype = new Parent();
+    return this;
+});
+
+var Cat = function (name) {
+    this.name = name;
+}.inherits(Mammal).method('purr', function(n){	
+	return 'purr';
+}).method('xname', function () {
+	return this.name;
+});
+
+var myCat = new Cat('Henrietta');
+console.log('myCat: '+ myCat.xname() + ' ' + myCat.says() + ' ' + myCat.purr(5));

d3e05742d05445964d71a035202f37a2ce73523e
Camila San <smayres@gmail.com>
2016-11-17 15:32:01 +0100

    Mod: removed unused lines

--- Sloth/mainwindow.cpp
+++ Sloth/mainwindow.cpp
@@ -123,11 +123,6 @@ void MainWindow::paintGL(){
     glDrawArrays(GL_TRIANGLES, 0, 3);
     glDrawArrays(GL_TRIANGLES, 1, 3);
 
-    // Set colors
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, mColorBuffer);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
     // Set triangles - this would be the plots
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, mVertexBuffer1);
